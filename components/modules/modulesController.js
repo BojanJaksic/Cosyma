2f5a6ea5bb5232dc5e33858bd77695541b009abc
BJaksic <bojan.jaksic@gmail.com>
2015-10-07 11:42:17 +0200

    working navigation

--- components/modules/modulesController.js
+++ components/modules/modulesController.js
@@ -1,8 +1,9 @@
 (function () {
     'use strict';
-    var ModulesController = function ($translate, $translatePartialLoader, $scope, modulesFactory, $stateParams) {
+    var ModulesController = function ($translate, $translatePartialLoader, $scope, modulesFactory, applicationsFactory, $stateParams) {
         $translatePartialLoader.addPart('modules');
         $translate.refresh();
+        $scope.applications = applicationsFactory.getApplications();
         $scope.modules = modulesFactory.getModulesBaseInfo($stateParams.applicationId);
         $scope.applicationName = $stateParams.applicationName;
         $scope.$parent.isApplicationView = false;
